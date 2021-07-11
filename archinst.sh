4bc63dd8706e4b1538c37d8dd66bee2f8827e99b
p-kolacz <piotr@kolacz.org>
2021-07-12 00:16:54 +0200

    Bootloader fix

--- archinst.sh
+++ archinst.sh
@@ -25,7 +25,7 @@ case $1 in
 			ucode="amd-ucode"
 		fi
 		pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware $ucode git stow sudo neovim fish man-db terminus-font
-		genfstab -U /mnt >> /mnt/etc/fstab
+		genfstab -U /mnt > /mnt/etc/fstab
 		read -p "Press any key..."
 		vim /mnt/etc/fstab
 		cp $0 /mnt/
@@ -47,26 +47,30 @@ case $1 in
 		while [[ -z $HOST ]]; do
 			read -p "Enter hostname: " HOST
 		done
-		echo -n $HOST > /etc/hostname
-		echo -e "127.0.0.1	localhost\n::1		localhost\n127.0.0.1	$HOST.localdomain $HOST\n" >> /etc/hosts
+		echo $HOST > /etc/hostname
+		echo -e "127.0.0.1	localhost\n::1		localhost\n127.0.0.1	$HOST.localdomain $HOST" >> /etc/hosts
 
 		echo "----- [Users] -----"
+		echo "Set password for root:"
 		passwd
 		useradd -m -G wheel -s "$USERSHELL" $USERNAME
+		echo "Set password for $USERNAME:"
 		passwd $USERNAME
 		echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/10_wheel
 
+		# TODO: move elsewere
 		echo "----- [Installing YAY] -----"
 		git clone https://aur.archlinux.org/yay.git /tmp/yay
 		cd /tmp/yay && makepkg -si
 		;;
 	bootloader)
 		echo "----- [Bootloader] -----"
-		# bootctl install
-		echo -e "timeout 0\ndefault arch" > boot/loader/loader.conf
-		echo -e "title Arch Linux\nlinux /vmlinuz-linux-lts\ninitrd /intel-ucode.img\ninitrd /initramfs-linux-lts.img" > boot/loader/entries/arch.conf
-		echo -e "options root=\"LABEL=$DISK_LABEL\" rw modprobe.blacklist=iTCO_wdt nowatchdog" >> boot/loader/entries/arch.conf
-		$EDITOR boot/loader/entries/arch.conf
+		bootctl install
+		echo -e "timeout 0\ndefault arch" > /boot/loader/loader.conf
+		echo -e "title Arch Linux\nlinux /vmlinuz-linux-lts\ninitrd /intel-ucode.img\ninitrd /initramfs-linux-lts.img" > /boot/loader/entries/arch.conf
+		echo -e "options root=\"LABEL=$DISK_LABEL\" rw modprobe.blacklist=iTCO_wdt nowatchdog" >> /boot/loader/entries/arch.conf
+		read -p "Press any key..."
+		$EDITOR /boot/loader/entries/arch.conf
 		;;
 	exit)
 		exit && umount -R /mnt
