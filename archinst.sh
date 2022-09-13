03cd8277278acb7dca9ffbbbf39f326f700d6d82
p-kolacz <piotr@kolacz.org>
2022-09-13 02:10:26 +0200

    Refactoring and bootloader install question

--- archinst.sh
+++ archinst.sh
@@ -1,47 +1,69 @@
 #!/bin/bash
 
-USER_NAME=piotr
-USER_SHELL=/bin/fish
+USER_SHELL=/usr/bin/zsh
 ROOT_LABEL=ARCH
 ESP_LABEL=ESP
 TIMEZONE=Europe/Warsaw
-EDITOR=nvim
+EDITOR=vim
+INITIAL_APPS=(base base-devel linux-lts linux-lts-headers linux-firmware git man-db terminus-font zsh zsh-autosuggestions zsh-syntax-highlighting)
+SCRIPT=${0##*/}
+
+if grep -q GenuineIntel /proc/cpuinfo; then
+	CPU="intel"
+elif grep -q AuthenticAMD /proc/cpuinfo; then
+	CPU="amd"
+fi
+[[ -z $CPU ]] && { echo "Can't determine CPU vendor, exiting."; exit 1; }
+
+wait_any_key() {
+	read -rs -n1 -p "${1:-Press any key...}"
+}
+
+prompt() {      # prompts for exacly one word
+	echo
+	local INPUT
+	while [[ ${#INPUT[@]} -ne 1 ]]; do
+		read -r -p "$1" -a INPUT
+	done
+	local -n ref=$2
+	# shellcheck disable=SC2034
+	ref=${INPUT[0]}
+}
 
 case $1 in
 	prepare)
 		[[ -d /sys/firmware/efi/efivars ]] && echo "UEFI mode" || echo "BIOS mode"
+		echo "${CPU^^} CPU detected"
 		[[ $(ping -c 1 archlinux.org) ]] || { echo "Network down. Use 'iwctl' to connect to WiFi."; exit 1; }
-		echo -e "Network is ready\n"
+		echo "Network is ready"
 		timedatectl set-ntp true
 		timedatectl status
-		echo -e "\n1. Use lsblk, gdisk to partition."
+		echo -e "\n1. Use lsblk, fdisk/gdisk to partition."
 		echo "2. Use mkfs.fat -n $ESP_LABEL and mkfs.ext4 -L $ROOT_LABEL to format."
+		echo "3. When ready, run ./$SCRIPT install"
 		;;
 	install)
 		mount /dev/disk/by-label/$ROOT_LABEL /mnt
 		mkdir /mnt/boot
 		mount /dev/disk/by-label/$ESP_LABEL /mnt/boot
 		reflector
-		vim /etc/pacman.d/mirrorlist
-		ucode=""
-		if $(grep -q GenuineIntel /proc/cpuinfo); then
-			ucode="intel-ucode"
-		elif $(grep -q AuthenticAMD /proc/cpuinfo); then
-			ucode="amd-ucode"
-		fi
-		pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware $ucode networkmanager git stow sudo neovim fish man-db terminus-font
+		wait_any_key
+		$EDITOR /etc/pacman.d/mirrorlist
+		pacstrap /mnt "${INITIAL_APPS[@]}" "$CPU-ucode"
 		genfstab -U /mnt > /mnt/etc/fstab
-		read -p "Press any key..."
-		vim /mnt/etc/fstab
-		cp $0 /mnt/
-		echo "Chrooting /mnt ..."
-		arch-chroot /mnt
+		wait_any_key
+		$EDITOR /mnt/etc/fstab
+		cp "$0" /mnt/
+		echo "Installaction complete. Run /$SCRIPT configure"
 		;;
 	configure)
+		echo "Chrooting /mnt ..."
+		arch-chroot /mnt
 		echo "----- [Time & Locale] -----"
 		ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 		hwclock --systohc
 		systemctl enable systemd-timesyncd.service
+		systemctl start systemd-timesyncd.service
 		sed -i -e "/#en_US.UTF-8/s/^#//" /etc/locale.gen
 		sed -i -e "/#pl_PL.UTF-8/s/^#//" /etc/locale.gen
 		locale-gen
@@ -49,42 +71,41 @@ case $1 in
 		echo -e "KEYMAP=pl\nFONT=ter-220b" > /etc/vconsole.conf
 
 		echo "----- [Network] -----"
-		while [[ -z $HOST ]]; do
-			read -p "Enter hostname: " HOST
-		done
-		echo $HOST > /etc/hostname
+		prompt "Enter hostname:" HOST
+		echo "$HOST" > /etc/hostname
 		echo -e "127.0.0.1	localhost\n::1		localhost\n127.0.0.1	$HOST.localdomain $HOST" >> /etc/hosts
-		systemctl enable NetworkManager.service
 
 		echo "----- [Users] -----"
 		echo "Set password for root"
 		passwd
-		useradd -m -G wheel -s "$USER_SHELL" $USER_NAME
+		prompt "Enter username: " USER_NAME
+		useradd -m -G wheel -s "$USER_SHELL" "$USER_NAME"
 		echo "Set password for $USER_NAME"
-		passwd $USER_NAME
+		passwd "$USER_NAME"
 		echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/10_wheel
+
+		prompt "Install bootloader (y/n)?" INSTALL_BL
+		[[ $INSTALL_BL == "y" ]] && {
+			# ;;
+		# bootloader)
+			echo "----- [Bootloader] -----"
+			bootctl install
+			echo -e "timeout 0\ndefault arch" > /boot/loader/loader.conf
+			echo -e "title Arch Linux\nlinux /vmlinuz-linux-lts\ninitrd /$CPU-ucode.img\ninitrd /initramfs-linux-lts.img" > /boot/loader/entries/arch.conf
+			echo -e "options root=\"LABEL=$ROOT_LABEL\" rw modprobe.blacklist=iTCO_wdt nowatchdog" >> /boot/loader/entries/arch.conf
+			wait_any_key
+			$EDITOR /boot/loader/entries/arch.conf
+		}
+		echo "exit or Ctrl+D and run $SCRIPT finish"
 		;;
-	bootloader)
-		echo "----- [Bootloader] -----"
-		bootctl install
-		echo -e "timeout 0\ndefault arch" > /boot/loader/loader.conf
-		echo -e "title Arch Linux\nlinux /vmlinuz-linux-lts\ninitrd /intel-ucode.img\ninitrd /initramfs-linux-lts.img" > /boot/loader/entries/arch.conf
-		echo -e "options root=\"LABEL=$ROOT_LABEL\" rw modprobe.blacklist=iTCO_wdt nowatchdog" >> /boot/loader/entries/arch.conf
-		read -p "Press any key..."
-		$EDITOR /boot/loader/entries/arch.conf
-		;;
-	exit)
-		echo "1. exit"
-		echo "2. umount -R /mnt"
-		echo "3. reboot"
+	finish)
+		echo "Unmounting /mnt"
+		umount -R /mnt || { echo "Did you exited chroot?"; exit 2; }
+		wait_any_key "All done! Press any key to reboot..."
+		reboot
 		;;
 	*)
 		echo "Arch auto installer"
-		echo "-------------------"
-		echo "archinst.sh prepare"
-		echo "archinst.sh install"
-		echo "archinst.sh configure"
-		echo "archinst.sh bootloader"
-		echo "archinst.sh exit"
+		echo "USAGE: $SCRIPT [prepare|install|configure|finish]"
 esac
 
